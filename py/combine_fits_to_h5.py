5a86beb1acaa718b65a84060bcd565e3abe383a3
Dominik Neise <neised@phys.ethz.ch>
2017-12-04 14:23:45 +0100

    next_version

---
+++ py/combine_fits_to_h5.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+from glob import glob
+from tqdm import tqdm
+import pandas as pd
+from fact.auxservices import AuxService
+
+
+def combine_fits_to_h5(
+    glob_expr='aux/*.MAGIC_WEATHER_DATA.fits'
+):
+    pd.concat(
+        [
+            AuxService.read_file(path)
+            for path in tqdm(sorted(glob(glob_expr)))
+        ],
+        ignore_index=True
+    ).to_hdf("MAGIC_WEATHER_DATA.h5", "MAGIC_WEATHER_DATA")
+
+if __name__ == "__main__":
+    combine_fits_to_h5()
