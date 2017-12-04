a715b5165c8f1399a19dc56765d858fc6fa72dd3
Dominik Neise <neised@phys.ethz.ch>
2017-12-04 14:36:39 +0100

    assign good index

--- py/combine_fits_to_h5.py
+++ py/combine_fits_to_h5.py
@@ -8,13 +8,17 @@ from fact.auxservices import AuxService
 def combine_fits_to_h5(
     glob_expr='aux/*.MAGIC_WEATHER_DATA.fits'
 ):
-    pd.concat(
+    df = pd.concat(
         [
             AuxService.read_file(path)
             for path in tqdm(sorted(glob(glob_expr)))
         ],
         ignore_index=True
-    ).to_hdf("MAGIC_WEATHER_DATA.h5", "MAGIC_WEATHER_DATA")
+    )
+    df['Time'] = pd.to_datetime(df.Time, unit='d')
+    df.set_index('Time', inplace=True)
+    df.sort_index(inplace=True)
+    df.to_hdf("MAGIC_WEATHER_DATA.h5", "MAGIC_WEATHER_DATA")
 
 if __name__ == "__main__":
     combine_fits_to_h5()
